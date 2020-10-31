4b2f1b1732d7ef7fca008ca5ef9faeff0334454e
Dany Dodson <danydodson@gmail.com>
2020-10-30 22:14:02 -0500

    configured netlify

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,12 +4,11 @@ const config = {
   siteTitleAlt: "Dany Dodson | @danydodson", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://danycodes.netlify.app", // Domain of your website without pathPrefix.
-  // siteUrl: "https://danycodes.herokuapps.com", // Domain of your website without pathPrefix.
   repo: "https://github.com/danydodson/dany.codes",
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "Hi, I’m Dany.😎 I help people to make better world by good apps, I am Publisher, Trainer Developer, working on Enterprise and open source Technologies JavaScript frameworks (React Angular 2.x), I work with client side and server side javascript programming", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteRssTitle: "Gatsby Advanced Starter RSS feed", // Title of the RSS feed
+  siteRssTitle: "@danydodson RSS feed", // Title of the RSS feed
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
   disqusShortname: "dany-codes", // Disqus shortname.
@@ -21,8 +20,8 @@ const config = {
   userTwitter: "danydodson", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "Midwest, USA", // User location to display in the author segment.
   userAvatar: "https://en.gravatar.com/userimage/62805797/de9073787b8f44890f332e259f2e1bbd.jpeg", // User avatar to display in the author segment.
-  // userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription: "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+  // 
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
